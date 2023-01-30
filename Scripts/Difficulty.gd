f0da956be2591838261334c54f5919c14a1c459a
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2023-01-30 17:26:02 +0200

    menu
    main menu + difficulty menu

---
+++ Scripts/Difficulty.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_Easy_pressed():
+	pass # Replace with function body.
+
+
+func _on_Suuuuuper_Easy_pressed():
+	pass # Replace with function body.
+
+
+func _on_Very_Easy_pressed():
+	pass # Replace with function body.
