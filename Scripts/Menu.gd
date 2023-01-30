237e94fe64f7cfbe55b7d3d7fada8012f3a24a9f
professorik <36134599+professorik@users.noreply.github.com>
2023-01-30 17:44:14 +0200

    tiny fixes

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -10,10 +10,8 @@ func _on_Settings_pressed():
 
 
 func _on_Help_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/Difficulty.tscn")
 
 
 func _on_Exit_pressed():
-	pass # Replace with function body.
-
-
+	get_tree().quit()
