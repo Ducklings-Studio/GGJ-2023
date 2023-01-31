84d11c3fabe1dfb30c3f06c58a3121077cfaf146
professorik <36134599+professorik@users.noreply.github.com>
2023-01-31 14:36:18 +0200

    click handler

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+export var delta = Vector2(2.5, 2.5) 
+
 var is_blocking = true
 
 func _ready():
 	AudioManager.set_music("res://Assets/Audio/MatchSound1.ogg")
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() and InputMap.event_is_action(event, "ui_left_mouse_button"):
+			var evpos = get_global_mouse_position() + delta
+			var coords = $floor.world_to_map(evpos)
+			$floor.set_cellv(coords, 4)
