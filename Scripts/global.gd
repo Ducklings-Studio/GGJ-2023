876fbb07f7a0480abdaf41de9dd744ae3ea78607
professorik <36134599+professorik@users.noreply.github.com>
2023-01-31 16:17:41 +0200

    buttons audios

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,10 +2,26 @@ extends Node
 
 
 func _ready():
+	var buttons: Array = get_tree().get_nodes_in_group("ui_buttons")
+	for i in buttons:
+		i.connect("mouse_entered", self, "play_button")
+		i.connect("pressed", self, "play_button_click")
+
 	var sliders: Array = get_tree().get_nodes_in_group("ui_sliders")
-	for inst in sliders:
-		inst.connect("value_changed", self, "play_slider")
+	for i in sliders:
+		i.connect("value_changed", self, "play_slider")
+
+
+func play_button_click():
+	print("1")
+	AudioManager.play("res://Assets/Audio/Effects/MenuButton.wav")
+
+
+func play_button():
+	print("2")
+	AudioManager.play("res://Assets/Audio/Effects/MenuButton.wav")
 
 
 func play_slider(_tmp):
-	AudioManager.play("res://Assets/Audio/Effects/click_004.ogg")
+	print("3")
+	AudioManager.play("res://Assets/Audio/Effects/MenuButton.wav")
