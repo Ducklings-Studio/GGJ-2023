285215d91368ef96f67fb0a5fc606e72ed9e6bd6
Art19em04 <70907971+Art19em04@users.noreply.github.com>
2023-02-01 21:27:59 +0200

    Simple duel map

--- Scripts/Difficulty.gd
+++ Scripts/Difficulty.gd
@@ -9,8 +9,8 @@ func _on_Suuuuuper_Easy_pressed():
 
 
 func _on_Very_Easy_pressed():
-	get_tree().change_scene("res://Scenes/Chill_Beach.tscn")
+	get_tree().change_scene("res://Scenes/Duelity.tscn")
 	
-	
+
 func _on_Back_pressed():
 	self.set_visible(false)
