5c2ba97a7b2fee7c29eae094753d707da94effa8
Art19em04 <70907971+Art19em04@users.noreply.github.com>
2023-02-02 14:10:01 +0200

    Difficulty update

--- Scripts/Difficulty.gd
+++ Scripts/Difficulty.gd
@@ -1,16 +1,17 @@
 extends Control
 
-func _on_Easy_pressed():
+func _on_Hard_pressed():
 	get_tree().change_scene("res://Scenes/Game.tscn")
 
 
-func _on_Suuuuuper_Easy_pressed():
+func _on_Normal_pressed():
 	get_tree().change_scene("res://Scenes/Chill_Beach.tscn")
 
 
-func _on_Very_Easy_pressed():
+func _on_Easy_pressed():
 	get_tree().change_scene("res://Scenes/Duelity.tscn")
 	
 
 func _on_Back_pressed():
 	self.set_visible(false)
+
