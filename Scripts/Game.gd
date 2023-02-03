f2ad98e880d07e6e9b05816ff3d0435e28f7380e
Art19em04 <70907971+Art19em04@users.noreply.github.com>
2023-02-03 14:14:21 +0200

    Base and some balance

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -27,12 +27,17 @@ var gems = 0
 var graph := {}
 var reversed_graph := {}
 
+#Base for duelity
+const BaseX = -1;
+const BaseY = -24;
 
 func _ready():
 	set_fogs()
 	add_gems(450)
-	build(Vector2.RIGHT * 5, 0)
-	build(Vector2(25, -5), 0)
+	
+	build(Vector2(BaseX, BaseY), 0)
+	build(Vector2(BaseX, -BaseY), 0)
+	
 	clean_action()
 	AudioManager.set_music("res://Assets/Audio/MatchSound.ogg")
 
@@ -200,16 +205,16 @@ func is_enough_gems(class_id: int):
 
 
 #Simple mushroom fog review
-const X_FOG_START = -7;
-const X_FOG_END = 8;
+const X_FOG_START = -6;
+const X_FOG_END = 6;
 const Y_FOG_START = -6;
-const Y_FOG_END = 7;
+const Y_FOG_END = 6;
 
 #Base mushroom fog review 
-const X_FOG_START_BASE = -8;
-const X_FOG_END_BASE = 9;
-const Y_FOG_START_BASE = -8
-const Y_FOG_END_BASE = 9;
+const X_FOG_START_BASE = -15;
+const X_FOG_END_BASE = 15;
+const Y_FOG_START_BASE = -15;
+const Y_FOG_END_BASE = 15;
 
 
 func removeFog(class_id: int, coords: Vector2, 
--- Scripts/Mushrooms/Defender.gd
+++ Scripts/Mushrooms/Defender.gd
@@ -1,5 +1,6 @@
 extends BasicMushroom
 class_name Defender
 
-
+export var min_build_radius: int = 3
+export var max_build_radius: int = 4
 
