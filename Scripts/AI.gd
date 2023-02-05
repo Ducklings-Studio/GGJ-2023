2cccc89ae76dab2ed28a62261a96140c86fdb051
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2023-02-05 15:24:18 +0200

    Update AI.gd

--- Scripts/AI.gd
+++ Scripts/AI.gd
@@ -129,7 +129,10 @@ func do_attack():
 		if mushroom is Attacker:
 			for target in targets:
 				if target.killer == i:
-					if get_parent().is_enough_gems(-1, gems, Global.ATTACK) and get_parent().can_attack(i, target.victim) and !(get_parent().get_mushroom(target.victim) is Defender):
+					if get_parent().is_enough_gems(-1, gems, Global.ATTACK) and get_parent().can_attack(i, target.victim):
+						if !(get_parent().get_mushroom(target.victim) is Defender):
+							targets.erase(target)
+							return
 						var fl = get_parent().attack(i, target.victim)
 						if fl:
 							add_gems(-Global.I_ATTACK.attack_price)
